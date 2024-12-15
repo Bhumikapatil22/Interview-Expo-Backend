ed1baa3e67fe82b0400584c67958a9a152a88e92
Bhumika Patil <148256877+Bhumikapatil22@users.noreply.github.com>
2024-12-15 18:30:13 +0530

    Update index.js

--- api/index.js
+++ api/index.js
@@ -10,7 +10,7 @@ import cors from 'cors';
 dotenv.config();
 const app=express();
 const corsOptions = {
-    origin: ['https://interview-expo.vercel.app/'], // Frontend URLs
+    origin: ['https://interview-expo.vercel.app'], // Frontend URLs
     methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
     credentials: true, // Allow cookies and credentials
   };
